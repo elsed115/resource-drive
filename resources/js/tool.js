5c3afca9b9eac44483f472fdaa05ac749bc44a5d
giulianodinardo <giulianodinardo@codeloops.it>
2025-07-18 15:44:43 +0200

    feat: add logging for debugging

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,8 +1,8 @@
-
 import '../css/tool.css'
 
 import Tool from './components/Tool.vue'
 
 Nova.booting(app => {
+  console.log('Resource Drive Tool: Booting Vue component.');
   app.component('resource-file-manager', Tool)
 })
--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Elsed115\ResourceDrive;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Laravel\Nova\Nova;
@@ -10,13 +11,18 @@ class ToolServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Log::info('ResourceDrive Tool: Booting Service Provider.');
+
         $this->app->booted(function () {
             $this->routes();
+            Log::info('ResourceDrive Tool: Routes registered.');
         });
 
         Nova::serving(function (ServingNova $event) {
+            Log::info('ResourceDrive Tool: ServingNova event caught.');
             Nova::script('resource-drive', __DIR__.'/../dist/js/tool.js');
             Nova::style('resource-drive', __DIR__.'/../dist/css/tool.css');
+            Log::info('ResourceDrive Tool: Assets registered.');
         });
     }
 
@@ -43,6 +49,6 @@ class ToolServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Log::info('ResourceDrive Tool: Registering Service Provider.');
     }
 }
