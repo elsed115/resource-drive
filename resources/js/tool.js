c0875979f0fef38631f998def439386321981769
giulianodinardo <giulianodinardo@codeloops.it>
2025-07-18 17:23:14 +0200

    R v1.2.0

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -4,5 +4,5 @@ import Tool from './components/Tool.vue'
 
 Nova.booting(app => {
   console.log('Resource Drive Tool: Booting Vue component.');
-  app.component('resource-file-manager', Tool)
+  app.component('resource-drive', Tool)
 })
